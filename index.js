ed74d762456c6aedd6edc16734b0e42d374ba752
jawwadturabi <memonjawwad9@gmail.com>
2019-09-07 20:58:34 +0500

    error solved

--- index.js
+++ index.js
@@ -1,83 +1,49 @@
-// var rq = require('request');
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express().use(bodyParser.json());
-// const baseUrl = "https://usil.instructure.com/api/v1/"
+'use strict';
 
-app.get("/webhook", (request, response) => {
-    var intent = request.params
-    console.log("request is : ", intent);
-    response.send({
-        "version": "v2",
-        "content": {
-          "messages": [
-            {
-              "type": "text",
-              "text": "simple text with button",
-              "buttons": [
-                {
-                  "type": "url",
-                  "caption": "External link",
-                  "url": "https://manychat.com"
-                }
-              ]
-            }
-          ],
-          "actions": [],
-          "quick_replies": []
-        }
-    })
-    
-})
-app.listen(process.env.PORT||4000,()=>{
-    console.log("server is running")
-})
-// var us_Id = request.params.usId
-    // var courseId = request.params.courseId
-    // var options = {
-    //     url: baseUrl + "users/" + us_Id + "/courses/" + courseId + "/assignments",
-    //     auth: {
-    //         'bearer': '11286~cKNZP8SsHdUgk2vfmXGV0J3mfKGe3uyuv664oinBASOMr3MfxhDx5vZ9nNnrniG4'
-    //     }
-    // };
-    // function callback(error, _res, body) {
-    //     console.log("url = ", options.url)
-    //     if (!error && response.statusCode == 200) {
-    //         console.log("body is : ", body)
-    //         var info = JSON.parse(body);
-    //         console.log("body is : ", info[0].id)
-    //         if (body) {
-    //             response.send(
-    //                 data(info)
-    //             )
-    //         }
-    //         else {
-    //             response.send({
-    //                 "messages": [
-    //                     {
-    //                         "text": "You are not enroll in any course",
-    //                         "quick_replies": [
-    //                             {
-    //                                 "title": "More",
-    //                                 "block_names": ["Principal"]
-    //                             },
-    //                             {
-    //                                 "title": "Go Back",
-    //                                 "block names": ["welcome message"]
-    //                             }
-    //                         ]
-    //                     }
-    //                 ]
-    //             })
-    //         }
-    //     }
-    //     else if (error) {
-    //         console.log('error is:', error);
-    //         response.send({
-    //             messages: {
-    //                 "text": "Something went wrong please try again with valid user id and course id"
-    //             }
-    //         })
-    //     }
-    // }
-    // rq(options, callback);
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
